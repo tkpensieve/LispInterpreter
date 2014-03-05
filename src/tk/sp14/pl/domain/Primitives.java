9828f53a56ac9c141f9af51ccaebe56db89589e4
Karthikeyan Thangavel <tkchennai89@gmail.com>
2014-03-05 02:21:20 -0500

    Minor changes in mathematical functions <Karthik>

--- src/tk/sp14/pl/domain/Primitives.java
+++ src/tk/sp14/pl/domain/Primitives.java
@@ -8,13 +8,13 @@ public class Primitives {
 	
 	public SExpression CAR(SExpression s) throws InvalidOperationException{
 		if(!(s instanceof ComplexSExpression))
-			throw new InvalidOperationException();
+			throw new InvalidOperationException("Error - CAR not defined for simple atoms");
 		return ((ComplexSExpression)s).getLeft();
 	}
 	
 	public SExpression CDR(SExpression s) throws InvalidOperationException{
 		if(!(s instanceof ComplexSExpression))
-			throw new InvalidOperationException();
+			throw new InvalidOperationException("Error - CDR not defined for simple atoms");
 		return ((ComplexSExpression)s).getRight();
 	}
 	
@@ -28,7 +28,7 @@ public class Primitives {
 	
 	public Atom EQ(SExpression a, SExpression b) throws InvalidOperationException{
 		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+			throw new InvalidOperationException("Error - EQ is defined only for simple atoms");
 		if(a.equals(b))
 			return T;
 		else
@@ -44,56 +44,56 @@ public class Primitives {
 	}
 	
 	public Atom PLUS(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - PLUS is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return new Atom(Integer.toString(valA+valB), AtomType.NUMBERS);
 	}
 	
 	public Atom MINUS(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - MINUS is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return new Atom(Integer.toString(valA - valB), AtomType.NUMBERS);
 	}
 	
 	public Atom LESS(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - LESS is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return valA < valB ? T : NIL;
 	}
 	
 	public Atom GREATER(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - GREATER is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return valA > valB ? T : NIL;
 	}
 	
 	public Atom TIMES(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - TIMES is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return new Atom(Integer.toString(valA*valB), AtomType.NUMBERS);
 	}
 	
 	public Atom QUOTIENT(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - QUOTIENT is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return new Atom(Integer.toString(valA/valB), AtomType.NUMBERS);
 	}
 	
 	public Atom REMAINDER(SExpression a, SExpression b) throws InvalidOperationException{
-		if(!(a instanceof Atom) || !(b instanceof Atom))
-			throw new InvalidOperationException();
+		if(!INT(a).equals(T) || !INT(b).equals(T))
+			throw new InvalidOperationException("Error - REMAINDER is defined only for integers");
 		int valA = Integer.parseInt(((Atom)a).getValue());
 		int valB = Integer.parseInt(((Atom)b).getValue());
 		return new Atom(Integer.toString(valA%valB), AtomType.NUMBERS);
--- src/tk/sp14/pl/error/InvalidOperationException.java
+++ src/tk/sp14/pl/error/InvalidOperationException.java
@@ -6,4 +6,17 @@ public class InvalidOperationException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String message;
+	
+	public InvalidOperationException(String message) {
+		super();
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
